d3709d78f5bb475f2d57323d28420f02f96e2a28
AhmedGamal40 <ag263809@gmail.com>
2023-07-06 20:57:33 +0300

    edite

--- controllers/authUserController.js
+++ controllers/authUserController.js
@@ -5,24 +5,30 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel2");
 
+// Singup
 exports.signup = asyncHandler(async (req, res, next) => {
+    // create
     const user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
     });
+    // generation token
     const token = jwt.sign({ userId: user._id }, process.env.jwt_Key, {
         expiresIn: process.env.jwt_ExpireDate,
     });
     res.status(201).json({ data: user, token });
 });
 
+//login
 exports.login = asyncHandler(async (req, res, next) => {
+    // check user exist , password ic correct
     const user = await User.findOne({ email: req.body.email });
     if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
         return next(new ApiError("incorrect Email or Password", 401));
     }
+    // generation token
     const token = jwt.sign({ userId: user._id }, process.env.jwt_Key, {
         expiresIn: process.env.jwt_ExpireDate,
     });
