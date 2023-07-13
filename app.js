bc1a1be6bb444caa38968ef657caa4f99ecd4b80
MinaOnsy24 <onsymina676@gamil.com>
2023-07-14 02:35:29 +0300

    mina

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors())
 // app.options('*',cors())
 // compress all responses
 app.use(compression())
-app.use(helmet());
+app.use(helmet( { crossOriginResourcePolicy: false}));
 
 //checkout webhook
 app.post('/Webhooks-checkout', express.raw({type: 'application/json'}),webhookCheckout)
