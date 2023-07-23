9f4626ec2a27e53732e453b73f0735843262ecc5
MinaOnsy24 <onsymina676@gamil.com>
2023-07-23 16:20:11 +0300

    idet mina

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -13,10 +13,10 @@ const AuthController = require("../controllers/authUserController");
 const router = express.Router();
 router.use(AuthController.protect);
 
-router.route('/checkout-session/:cartId').get(createStripeSession)
+router.route('/checkout-session/:cartId').get(AuthController.allowedTo("user","seller"),createStripeSession)
 router.put('/:id/pay',AuthController.allowedTo('admin'),updateOrderToPaid)
 router.put('/:id/delivered',AuthController.allowedTo('admin'),updateOrderToDelivered)
-router.route("/:cartId").post(AuthController.allowedTo("user"),cashOrder);
+router.route("/:cartId").post(AuthController.allowedTo("user","seller"),cashOrder);
 router.get(
     "/",
     AuthController.allowedTo("user","admin"),
